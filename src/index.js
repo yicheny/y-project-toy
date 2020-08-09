6907fed134537404c83b98af23dd55aef049cb97
YLF <1463020554@qq.com>
2020-08-10 01:04:58 +0800

    add 支持live2d

--- src/index.js
+++ src/index.js
@@ -5,6 +5,18 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './index.scss';
 import 'y-ui0/lib/style.css';
+import {L2Dwidget} from "y-live2d-widget/entry/src";
+
+L2Dwidget.init({
+    dialog: {
+        enable: true,
+        script: {
+            'every idle 10s': '$hitokoto$！', //显示动漫台词【使用一言语句】
+            'tap body': '哎呀！别碰我！',
+            'tap face': '人家已经不是小孩子了~'
+        }
+    }
+});
 
 ReactDOM.render(
   <React.StrictMode>
