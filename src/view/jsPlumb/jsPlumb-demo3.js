43e88dfb7c645626cd4e8c551e49c54e3e574852
yicheny <1463020554@qq.com>
2020-10-12 15:30:38 +0800

    update jsPlumb-demo3.js

--- src/view/jsPlumb/jsPlumb-demo3.js
+++ src/view/jsPlumb/jsPlumb-demo3.js
@@ -1,7 +1,12 @@
 import React, {useEffect} from 'react';
-import _ from 'lodash';
 import {jsPlumb} from 'jsplumb'
 import './jsPlumb-demo3.scss';
+
+const content = [
+    <div key={0} id={`item-${1}`} className="state-item">State 1</div>,
+    // <div key={1} id={`item-${2}`} className="state-item">State 2</div>,
+    <div key={2} id={`item-${3}`} className="state-item" style={{marginTop:300}}>State 3</div>,
+]
 
 function JsPlumbDemo3(props) {
 
@@ -11,8 +16,27 @@ function JsPlumbDemo3(props) {
         plumbIns.connect({
             source: 'item-1',
             target: 'item-3',
-            anchor: ['Left', 'Right', 'Top', 'Bottom', [0.3, 0, 0, -1], [0.7, 0, 0, -1], [0.3, 1, 0, 1], [0.7, 1, 0, 1]],
-            connector:['Flowchart',{alwaysRespectStubs:true,midpointer:0.2}],
+
+            //position
+            //只有一个的时候：箭头指向元素的位置
+            //有两个的时候：第一个是流出的位置，第二个是流入的位置
+
+            //x 箭头的左右偏移 >0向右
+            //y 箭头的上下偏移 >0向下 <0向上
+            //dx -1向右指向 +1向左指向
+            //dy -1向下指向 +1向上指向
+            //dx dy配合可用于流程线决定开始或结束一截的指向
+            //ox 线段的左右位置偏移 >0向右
+            //oy 线段的上下位置偏移 >0向下
+
+            // anchor: ['Right', [1,0.5,1,0]],
+            // anchor: ['Right','Right']
+            // anchor:[[-1,0.5,0,0],[1,0.5,1,0]],
+            // anchor:[[1,0.5,1,0]],
+            // anchor:[1,0.5,1,0],
+
+            anchor:[1,0.5,1,0,0,0],
+            connector:['Flowchart',{stub:120}],
             endpoint: 'Blank',
             paintStyle: { stroke: '#909399', strokeWidth: 2 },
             overlays: [ ['Arrow', { width: 8, length: 8, location: 1 }] ] ,
@@ -24,11 +48,7 @@ function JsPlumbDemo3(props) {
     return (
         <div className='JsPlumbDemo3'>
             <div className="wrapper">
-                {
-                    _.times(3,i=>i+1).map(x=>{
-                        return <div key={x} id={`item-${x}`} className="state-item">State {x}</div>
-                    })
-                }
+                { content }
             </div>
         </div>
     );
