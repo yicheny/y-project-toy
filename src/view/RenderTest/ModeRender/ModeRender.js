cb69c582af10fe840b3bcfa533fa7aa9bdf4bf1d
YLF <1463020554@qq.com>
2021-02-21 13:35:45 +0800

    add ModeRender测试

--- src/view/RenderTest/ModeRender/ModeRender.js
+++ src/view/RenderTest/ModeRender/ModeRender.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Button} from "y-ui0";
 
 function ModeRender(props) {
+    const [mode,setMode] = useState('A');
+
     return (<div>
-        ModeRender
+        <Button onClick={()=>setMode(mode==='A'?'B':'A')}>切换容器</Button>
+        {getBox(mode)}
     </div>);
 }
 
 export default ModeRender;
+
+function getBox(mode){
+    const boxMap = {
+        A:<BoxA/>,
+        B:<BoxB/>
+    }
+    // console.log('getBox');
+    return boxMap[mode];
+}
+
+function BoxA(){
+    console.log('BoxA')
+    return <div>BoxA</div>
+}
+
+function BoxB(){
+    console.log('BoxB')
+    return <div>BoxB</div>
+}
+
