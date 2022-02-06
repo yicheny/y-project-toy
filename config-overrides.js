2b1088d9d5b633e09aa9b55c88f94ca8b1e1aa0a
yicheny <ylvfeng@gmail.com>
2022-02-06 16:59:13 +0800

    add 使用esbuild和pnpm

---
+++ config-overrides.js
@@ -0,0 +1,20 @@
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+const {ESBuildPlugin} = require('esbuild-loader')
+
+const swp = new SpeedMeasurePlugin();
+
+module.exports = function (config,dev) {
+    config = swp.wrap(config)
+    config.plugins.push(new ESBuildPlugin());
+    const rules = config.module.rules[2].oneOf;
+    //使用esbuild-loader替换babel-loader
+    rules.splice(2,1,{
+        test:/\.(js|mjs)$/,
+        loader:'esbuild-loader',
+        options:{
+            loader:"jsx",
+            target:"es2015"
+        }
+    })
+    return config;
+}
